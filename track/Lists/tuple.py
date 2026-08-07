17f4651ea13e8fa3b01ebc188f278378e3ac9638
Vinodini Ilam <vinodiniilam@gmail.com>
2026-08-07 15:38:16 +0530

    Create And Unpack the Tuple

---
+++ track/Lists/tuple.py
@@ -0,0 +1,9 @@
+name=input("enter the name")
+course=input("enter the course")
+score=int(input("enter the score"))
+student_trecord=(name,course,score)
+Name,Course,Score=student_trecord
+print(name)
+print(course)
+print(score)
+print(student_trecord)
