ff431ef87c7fd8edf42701c128d3f74b65aa3d24
Vinodini Ilam <vinodiniilam@gmail.com>
2026-08-20 15:41:41 +0530

    using getter and setter methods

--- track/OOPS/class.py
+++ track/OOPS/class.py
@@ -1,28 +1,17 @@
 class Student:
-    def display(self):
-        print("rollno:", self.rollno)
-        print("name:", self.name)
-        print("age:", self.age)
-        print("marks:", self.marks)
+    def __init__(self,roll,name):
+        self.__roll=roll if roll>0 else "invalid roll no"
+        self.__name=name
+    def setroll(self,rollno): 
+        self.__roll=roll
+    def getroll(self):
+        return self.__roll
+    def setname(self,name):
+        self.__name=name    
+    def getname(self):
+        return self.__name 
+s1=Student(-11,"vinnu")
+print(s1.getroll())
+print(s1.getname())           
+     
 
-    def study(self):
-        print(self.name, "is Studied")
-
-
-s1 = Student()
-s1.rollno = 21
-s1.name = "vinnu"
-s1.age = 19
-s1.marks = 98
-
-s2 = Student()
-s2.rollno = 22
-s2.name = "siri"
-s2.age = 28
-s2.marks = 90
-
-s1.display()
-s1.study()
-
-s2.display()
-s2.study()
